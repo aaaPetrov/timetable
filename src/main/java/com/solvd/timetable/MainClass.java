73830e43540a99718c164e1fdeee15f2cbfc2f46
kchernopiatova <chernopiatovaks@gmail.com>
2021-12-13 13:32:36 +0300

    Main was cleaned

--- src/main/java/com/solvd/timetable/MainClass.java
+++ src/main/java/com/solvd/timetable/MainClass.java
@@ -1,11 +1,5 @@
 package com.solvd.timetable;
 
-import com.solvd.timetable.domain.GradeCurriculum;
-import com.solvd.timetable.persistence.GradeCurriculumRepository;
-import com.solvd.timetable.persistence.Impl.GradeCurriculumRepositoryImpl;
-
-import java.util.List;
-
 public class MainClass {
 
     public static void main(String[] args) {
