56603f32f1d97ace460e18a675a5b36fa7f6ad3e
Artem Petrov <PitPeacePolice@gmail.com>
2021-12-16 00:05:16 +0300

    Code was fixed.

--- src/main/java/com/solvd/timetable/MainClass.java
+++ src/main/java/com/solvd/timetable/MainClass.java
@@ -1,30 +1,18 @@
 package com.solvd.timetable;
 
 import com.solvd.timetable.algorithm.Algorithm;
-import com.solvd.timetable.domain.*;
 import com.solvd.timetable.domain.timetable.TimeTable;
-import com.solvd.timetable.service.*;
-import com.solvd.timetable.service.impl.*;
 
-import java.util.List;
 import java.util.Scanner;
 
 public class MainClass {
 
     public static void main(String[] args) {
-        GradeService gradeService = new GradeServiceImpl();
-        List<Grade> grades = gradeService.getAll();
-        RoomService roomService = new RoomServiceImpl();
-        List<Room> rooms = roomService.getAll();
-        TeacherService teacherService = new TeacherServiceImpl();
-        List<Teacher> teachers = teacherService.getAll();
-        GradeCurriculumService gradeCurriculumService = new GradeCurriculumServiceImpl();
-        List<GradeCurriculum> gradeCurricula = gradeCurriculumService.getAll();
-
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter the number of days in the school week(5 or 6): ");
         final int daysInWeek = scanner.nextInt();
-        Algorithm algorithm = new Algorithm(daysInWeek, grades, teachers, rooms, gradeCurricula);
+
+        Algorithm algorithm = new Algorithm(daysInWeek);
         TimeTable timeTable = algorithm.createTimeTable();
     }
 
