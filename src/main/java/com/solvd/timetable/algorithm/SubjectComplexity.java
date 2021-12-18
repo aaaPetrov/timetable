f608285fb1364dde9c324ffb5431d86b933697d1
Artem Petrov <PitPeacePolice@gmail.com>
2021-12-19 01:22:09 +0300

    Code was fixed.

---
+++ src/main/java/com/solvd/timetable/algorithm/SubjectComplexity.java
@@ -0,0 +1,37 @@
+package com.solvd.timetable.algorithm;
+
+import com.solvd.timetable.domain.Subject;
+
+public class SubjectComplexity {
+
+    private Long id;
+    private Subject subject;
+    private int complexity;
+
+    public SubjectComplexity() {}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Subject getSubject() {
+        return subject;
+    }
+
+    public void setSubject(Subject subject) {
+        this.subject = subject;
+    }
+
+    public int getComplexity() {
+        return complexity;
+    }
+
+    public void setComplexity(int complexity) {
+        this.complexity = complexity;
+    }
+
+}
